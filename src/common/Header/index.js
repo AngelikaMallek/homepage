ae0733eb44c91dc1bb0bccd1a5949c2898f6647d
Angelika Mallek <angelika.mallek03@gmail.com>
2023-11-17 10:54:50 +0100

    added person description

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,10 +1,19 @@
-import { WrapperImage, StyledHeader, WrapperDescription } from "./styled";
+import { WrapperImage, StyledHeader, WrapperDescription, Paragraph, Name, Description, Button } from "./styled";
+import envelope from "./envelope.svg";
 
 const Header = () => {
     return(
         <StyledHeader>
             <WrapperImage />
-            <WrapperDescription>test</WrapperDescription>
+            <WrapperDescription>
+                <Paragraph>THIS IS</Paragraph>
+                <Name>Angelika Mallek</Name>
+                <Description>👨🏻‍💻  I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities.</Description>
+                <Button>
+                    <img src={envelope} alt="EnvelopeIcon"></img>
+                    Hire Me
+                </Button>
+            </WrapperDescription>
         </StyledHeader>
     )
 }
--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -5,6 +5,18 @@ export const StyledHeader = styled.header`
     margin-top: 115px;
     display: flex;
     gap: 66px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        flex-direction: column;
+        gap: 32px;
+        margin-top: 32px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        flex-direction: column;
+        gap: 12px;
+        margin-top: 32px;
+    }
 `;
 
 export const WrapperImage = styled.div`
@@ -23,5 +35,55 @@ export const WrapperImage = styled.div`
 `;
 
 export const WrapperDescription = styled.div`
+    padding-top: 68px;
+    max-width: 624px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        padding-top: 0;
+    }
+`;
+export const Paragraph = styled.p`
+    color: ${({ theme }) => theme.colors.slateGray};
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 130%;
+    text-transform: uppercase;
+`;
+export const Name = styled.h1`
+    color: ${({ theme }) => theme.colors.mineShaft};
+    font-size: 38px;
+    font-weight: 900;
+    letter-spacing: 1.9px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 22px;
+        letter-spacing: 1.1px;
+    }
+`;
+export const Description = styled.p`
+    color: ${({ theme }) => theme.colors.mineShaft};
+    font-size: 20px;
+    line-height: 140%;
+    letter-spacing: 1px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) { 
+        font-size: 17px;
+        letter-spacing: 0.85px;
+    }
+`;
+export const Button = styled.button`
+    color: ${({ theme }) => theme.colors.white};
+    display: flex;
+    padding: 12px 16px;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    border-radius: 4px;
+    border: 1px solid var(--diff-bg-neutral, rgba(209, 213, 218, 0.30));
+    background-color: ${({ theme }) => theme.colors.scienceBlue};
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        gap: 12px;
+    }
 `;
 
