b4d65ff3ab4297867e7db1817d67df1d162d6d04
Angelika Mallek <angelika.mallek03@gmail.com>
2023-11-24 10:45:08 +0100

    changed style in portfolio

--- src/features/PersonalHomepage/Portfolio/PortfolioItem/styled.js
+++ src/features/PersonalHomepage/Portfolio/PortfolioItem/styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const PortfolioItemStyled = styled.div`
-    max-width: 592px;
     border-radius: 4px;
     border: 6px solid var(--diff-bg-neutral, rgba(209, 213, 218, 0.30));
     box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03), 0px -2px 50px 0px rgba(9, 10, 51, 0.02);
--- src/features/PersonalHomepage/Portfolio/styled.js
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -5,6 +5,11 @@ export const PortfolioWrapper = styled.div`
     grid-template-columns: auto auto;
     grid-gap: 32px;
 
+    @media(max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        grid-template-columns: auto;
+        grid-gap: 16px;
+    }
+
     @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
         grid-template-columns: auto;
         grid-gap: 16px;
